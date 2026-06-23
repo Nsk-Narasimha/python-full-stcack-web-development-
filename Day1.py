122c713a9f4900630ae5962dccd6a724461122ca
Narasimha Sai Kumar Kosuri <165867894+Nsk-Narasimha@users.noreply.github.com>
2026-06-23 06:49:40 +0530

    Day 1 practice files
    Contains basic Python syntax, variables, and first data types exercises from Day 1.

---
+++ Day1.py
@@ -0,0 +1,79 @@
+'''Procedural language
+step by step approch,hwre code is structure into procedures such as fuction or routinue...eg:c
+object oreiented language
+this is based on concepts of object and class...eg:java,python
+>>dynamicaly typed-python knows the tupe of data we are passing to the variable...
+>>interperter-python execute code line by line ,if any error it will stop the execution
+>>high level_1991,,guide van russum
+why use python?
+*easy syntax
+*cross platform
+*wide application
+*huge library support
+
+tokens
+>>keywords-there are reservedword in python....eg:if,else,type,int
+>>identifers
+>>literals
+>>operators
+>>punctuators
+
+rules of variable
+good way
+num_1=1
+num=4
+_num=9
+bad way
+2num=0
+nu m=0
+nu#m=9
+comments
+single line-#
+multi line_''' ''',""" """
+
+'''
+print(type(int))
+num=89
+print(id(num))
+print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
