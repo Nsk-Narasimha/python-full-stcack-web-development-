217f07e3b49c9c50c7c3b1b2bc289825d0cd2aaf
Narasimha Sai Kumar Kosuri <165867894+Nsk-Narasimha@users.noreply.github.com>
2026-06-29 12:22:22 +0530

    Day 6 practice files
    Contains basic Type conversions-int,float,string,list,tuple and biult in functions exercises from Day 6.

---
+++ Day6.py
@@ -0,0 +1,35 @@
+"""type converstions
+int-->float,string
+float-->int,string
+string-->int,tuple,float,list
+list(=-->string,tuple,dictionary
+tuple-->string,list
+
+"""
+a=7
+print(float(a))
+print(str(a))
+a=6.7
+print(int(a))
+print(str(a))
+a="234"
+print(int(a))
+a="34.567"
+print(float(a))
+a="nsk{ thr"
+print(list(a))
+print(tuple(a))
+a=["9","n"]
+print(str(a))
+b="".join(a)
+print(b)
+print(tuple(a))
+a=[(9,8),(7,8)]
+print(dict(a))
+a=("9","n")
+print(str(a))
+b="".join(a)
+print(list(a))
+
+
+
