51d92a3990d93f46b8fec5bc7d3df6300bfdf3e8
Narasimha Sai Kumar Kosuri <165867894+Nsk-Narasimha@users.noreply.github.com>
2026-07-02 12:21:19 +0530

    Day 9 practice files
    Contains basic statements (control statements and loops) with all data types exercises from Day 9.

---
+++ Day9.py
@@ -0,0 +1,23 @@
+""" Loops
+for loop _ for itterations
+while loop
+-------
+control statements
+break,continue,pass-space holder
+---------
+assert keyword is check the condition, but it will raise a error in case it is false
+
+"""
+a="fg"
+for b in a:print(b)
+b=[1,2,3]
+for v in b:
+    if(v==2):
+        continue
+    print(v)
+else:print("program ended")#for else is run after it unitil is there any break in case it will not run
+for i in range(1,51):print(i,end=" ")
+b=17
+assert b>=18,"your must be above 18 "
+
+
