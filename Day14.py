e6457ce3dd08fe5a1424c9e483ce3c58522eb499
Narasimha Sai Kumar Kosuri <165867894+Nsk-Narasimha@users.noreply.github.com>
2026-07-09 11:15:48 +0530

    Day 14 practice files
    Contains basic functions(Lambda function, filter keyword, list comperhensions, dictionary comperhension)from Day 14.

---
+++ Day14.py
@@ -0,0 +1,30 @@
+"""Lambda function
+lamdba(parameters):return
+
+Some=lambda an,so,why:an+so+why
+print(Some(10,3,5))
+
+filter(function,itterable)only list,tuple,set
+
+nums=[1,2,3,4,5]
+rev=filter(lambda a:a%2==0,nums)
+print(set(rev))
+
+list comperhensions
+
+old=[1,2,3,4,5,6]
+new=[j for j in old if j%2==0]
+print(new)
+old=[1,2,3,4,5,6]
+new=[j for j in old]
+print(new)
+
+dictionary comperhension
+
+old={1:2,3:7,5:6}
+new={i:j for(i,j)in old.items() if j%2==0}
+print(new)
+
+
+
+"""
