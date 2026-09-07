84a282b7a10ba6f6b0e04b1373671c3b9ab2cdac
Nsk-Narasimha <knsknsk10@gmail.com>
2026-09-07 13:16:50 +0530

    RecapOfDtatypes

---
+++ dsa-1.py
@@ -0,0 +1,18 @@
+email='saketha@codegnan.com'
+print(email[8:16])
+email_id=['knsknsk10@gmail.com','narasimha@gmail.com','sai_kumar546@yahoo.com','info@codegnan.com']
+print(len(email_id))
+print(*email_id[-1:-3:-1])
+email_id.extend(['abc@gmail.com','xyz@gmail.com'])
+print(email_id)
+for mail in email_id:
+    print(f'the person is {mail}')
+users={}
+print(type(users))
+for i in range(len(email_id)):
+    users[i+1]=email_id[i]
+
+print(*enumerate(email_id,1))
+#users=dict.(email_id)
+
+
